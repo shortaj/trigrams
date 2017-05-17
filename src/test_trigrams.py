189a2270f0358e6324ab0b432cc8f167f9fef587
Alex <ajshort2010@hotmail.com>
2017-05-17 15:22:59 -0700

    Some additions to test.

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -1,7 +1,15 @@
 """A file to test the functions of trigrams."""
 import pytest
 
+SAMPLE_TXT = ''
 
-@pytest.mark.parameterize('path', 'word_count')
-def test_holder(path, word_count):
+
+@pytest.mark.parametrize('file_path', SAMPLE_TXT)
+def test_get_text(file_path):
+    """A test function for get_text."""
+    pass
+
+@pytest.mark.parametrize('text_list', SAMPLE_DICT)
+def test_generate_trigrams(text_list):
+    """A test function for generate_trigrams."""
     pass
