de3736bc768b81ab594486eeac1fb070a23c5531
Alex <ajshort2010@hotmail.com>
2017-05-17 15:14:19 -0700

    Cleaned up linter.

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,26 +1,31 @@
-"""This module implements the trigrams algorithm"""
+"""This module implements the trigrams algorithm."""
 
 import io
 
+
 def main(file_path):
-    return generateTrigrams(getText(file_path))
+    """A function that calls get_text and generate_trigrams returns dict."""
+    return generate_trigrams(get_text(file_path))
 
 
-def getText(file_path):
-    """Get file and returns words in a list"""
+def get_text(file_path):
+    """Get file and returns words in a list."""
     f = io.open(file_path, encoding='utf-8')
     return f.read().lower().split()
 
-def generateTrigrams(textList):
+
+def generate_trigrams(text_list):
+    """A function that returns a dict based on the get_text file."""
     trigram_dict = {}
-    for index, word in enumerate(textList[:-2]):
-        key = word + ' ' + textList[index + 1]
+    for index, word in enumerate(text_list[:-2]):
+        key = word + ' ' + text_list[index + 1]
         if key in trigram_dict:
             value = trigram_dict.get(key)
-            value.append(textList[index + 2])
+            value.append(text_list[index + 2])
         else:
-            trigram_dict.setdefault(key, [textList[index + 2]])
+            trigram_dict.setdefault(key, [text_list[index + 2]])
 
     return trigram_dict
+
 
 print(main('./text.txt'))
